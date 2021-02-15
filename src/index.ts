c2bf16a4f755dcd7d6a999608e80cec794beb288
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-15 14:09:00 +0000

    Added depts to index

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import * as Conf from './lib/models/conf.model';
 import * as Env from './lib/Env';
 import * as Logger from './lib/Logger';
 import { initStopHandler } from './lib/ProcSigHandlers';
+import * as JwtWrapper from './lib/JwtWrapper';
 
-
-export { Conf, Env, Logger, initStopHandler }
+export { Conf, Env, Logger, JwtWrapper, initStopHandler }
