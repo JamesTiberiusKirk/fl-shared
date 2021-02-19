3a24b60c04ebde4ea3d7a03eaaf9bce739630e5d
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-19 09:55:09 +0000

    Version bump and cleaup

--- src/lib/misc/index.ts
+++ src/lib/misc/index.ts
@@ -1,8 +1,10 @@
 import * as morgan from 'morgan';
 import { Env } from '../../index';
 
+/**
+ * Returns an instance of morgan middleware.
+ */
 export function GetMorganMiddleware(){
-    //const msName = process.env.MS_NAME?.toUpperCase();
     const msName = Env.getMsName().toUpperCase();
     const morganTemplate = `[${msName}]: [HTTP]: :method :url :status :res[content-length] kb - :response-time ms`;
     return morgan(morganTemplate);
