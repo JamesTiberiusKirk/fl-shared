e72aef4f417e5883590f2471add8ba76624de3ec
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-21 07:34:53 +0000

    Updated main index

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as Env from './lib/Env';
 import * as Logger from './lib/Logger';
 import { initStopHandler } from './lib/ProcSigHandlers';
 import * as JwtWrapper from './lib/JwtWrapper';
-import { GetMorganMiddleware } from './lib/misc';
+import * as Misc from './lib/misc';
 
-export { Conf, Env, Logger, JwtWrapper, initStopHandler, GetMorganMiddleware }
+export { Conf, Env, Logger, JwtWrapper, initStopHandler, Misc }
