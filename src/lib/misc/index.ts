c10810146cdddef6c814c18c2d31fc1f17123fe7
Dumitru Vulpe <dumitru.v.dv@gmail.com>
2021-02-21 07:19:43 +0000

    Added timestamp function

--- src/lib/misc/index.ts
+++ src/lib/misc/index.ts
@@ -4,8 +4,15 @@ import { Env } from '../../index';
 /**
  * Returns an instance of morgan middleware.
  */
-export function GetMorganMiddleware(){
+export function GetMorganMiddleware() {
     const msName = Env.getMsName().toUpperCase();
     const morganTemplate = `[${msName}]: [HTTP]: :method :url :status :res[content-length] kb - :response-time ms`;
     return morgan(morganTemplate);
+}
+
+/**
+ * Returns a timestamp of the current moment.
+ */
+export function GenTimeStamp(): number {
+    return (Date.now() / 1000);
 }
